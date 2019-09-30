7956dd4d7136e2178cf5bd65e62285dec181ae99
Konstantin <rkonstantin@gmail.com>
2019-09-30 20:55:54 +0300

    new map

---
+++ rock-the-jvm-scala-beginners/src/lectures/part4Collections/Sequence.scala
@@ -0,0 +1,60 @@
+package lectures.part4Collections
+
+object Sequence extends App{
+
+  ///Seq
+
+  val aSequence = Seq(1,2,3,4,5)
+  val bSeq = aSequence ++ Seq(8,6,7)
+  println(aSequence.head)
+  println(aSequence.tail)
+  println(aSequence)
+  println(aSequence(2))
+  println(0+: aSequence :+ 10)
+  println(bSeq.sorted)
+  val ball4 = Seq.fill(4)("ball")
+  println(ball4)
+
+/// Range
+
+  val aRange: Seq[Int] = 1 to 10
+
+  aRange.foreach(println)
+  (1 to 10).foreach(x=>println("Hello"))
+
+  /// List
+
+  val list = List(1,2,3,4)
+  val prep = 42 +: list :+ 89
+
+  println(list)
+  println(42 :: list)
+  println(prep)
+  println(list(3))
+
+  val apples5 = List.fill(5)("apple")
+  println(apples5)
+  println(list.head)
+  println(list.tail)
+
+  println(prep.mkString(" "))
+
+  /// Arrays
+
+  val arr = Array[Int](1,2,3,4)
+  val arr1 = Array(1,2,3,4,5)
+  val arr2 = Array.ofDim[String](3)
+
+  arr.foreach(println)
+  arr1.foreach(println)
+  arr2.foreach(println)
+
+  val number :Seq[Int] = arr
+  println(number)
+  println(arr1.toSeq)
+  arr1(1) = 22
+  println(arr1.toSeq)
+
+
+
+}
---
+++ rock-the-jvm-scala-beginners/src/lectures/part4Collections/TuplesAndMaps.scala
@@ -0,0 +1,61 @@
+package lectures.part4Collections
+
+object TuplesAndMaps extends App{
+
+  // tuples
+  val aTuples = (2, "Hello Scala")
+  println(aTuples)
+  val bTuples= aTuples.copy(2,"Goodbuy Java")
+  val cTuples= aTuples.copy(_1 = 1)
+  println(bTuples)
+  println(cTuples)
+  println(bTuples.swap)
+
+  // map
+
+  val aMap : Map[String, Int] = Map()
+  val phonebook: Map[String,Int] = Map(("Jim",555), "Marry" ->757).withDefaultValue(-1)
+
+  //a -> b is suger (a,b)
+
+  println(phonebook)
+
+  // map aps
+
+  println(phonebook.contains("Jim"))
+  println(phonebook.contains("Axel"))
+  println(phonebook("Jim"))
+
+  // add pair
+
+  val pair1 = "Kos"-> 127
+  val pair2 = Map("Petr"->651, "Sasha"->652)
+  val newPhonebook = phonebook + pair1
+  val newPhonebook2 = newPhonebook ++ pair2
+  println(newPhonebook)
+  println(newPhonebook2)
+
+  // map, filter, flashMap
+
+ // println(phonebook.map{case(key,value)=>(key.)}
+  println(phonebook.map{ case (key, value) => (key.toLowerCase, value)})
+  println(newPhonebook2.map(pair=>pair._1.toLowerCase ->pair._2))
+  println(newPhonebook2.map(x=>(x._1.toLowerCase, x._2)))
+  println(newPhonebook2.map(x=>(x._1.toLowerCase)))
+  println(newPhonebook2.map(x=>(x._2 + 1)))
+
+// filter
+
+  println(newPhonebook2.filterKeys(x=> x.startsWith("J")).mapValues(v=>"1uu1 " + v))
+  println(newPhonebook2.filter(pair=> pair._1.startsWith("K")))
+
+// conversion toList or toMap
+
+  println(newPhonebook2.toList)
+
+  val nameList = List("Andrey", "Anton", "Jim", "Bob","Bil")
+  println(nameList.groupBy(name=>name.charAt(0)))
+
+
+
+}
