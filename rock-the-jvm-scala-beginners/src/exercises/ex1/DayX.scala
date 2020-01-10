82d93f6556234ef84da8037476e15567b53ac656
rehkalainin <rkonstantin@gmail.com>
2020-01-10 13:28:33 +0200

    after progon

--- rock-the-jvm-scala-beginners/src/exercises/ex1/DayX.scala
+++ rock-the-jvm-scala-beginners/src/exercises/ex1/DayX.scala
@@ -1,6 +1,27 @@
 package exercises.ex1
 
-object DayX extends App {
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 
+object DayX {
+
+  def op(n: Int) = Future {
+    Thread.sleep(1000)
+    n
+  }
+
+  // TODO t2 - t1 = ???
+  // TODO сколько потоков
+  def main( args: Array[String]) = {
+
+    // t1
+    val r = for {
+      n1 <- op(5)
+      n2 <- op(7)
+    } yield n1 + n2
+    // t2
+
+  }
 
 }
