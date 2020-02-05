636d034e9b9f0e2cdc9e9d5070fcde0b31b1ab6a
rehkalainin <rkonstantin@gmail.com>
2020-02-05 23:15:44 +0200

    seq 1 2 3

--- rock-the-jvm-scala-beginners/src/exercises/CheckDay.scala
+++ rock-the-jvm-scala-beginners/src/exercises/CheckDay.scala
@@ -1,7 +1,25 @@
 package exercises
 
 object CheckDay extends App {
+  /*
 
+    Дана последовательность val in = Seq(1, 2, 3, -1, -2, 1, -1, -2, -3)
+    нужно посчитать количество положительных и отрицательных последовательностей
+    которых больше 3
+
+     */
+
+  val in = Seq(1, 2, 3, -1, -2, 1, -1, -2, -3)
+
+  val res1 = in.foldLeft((0, 0, 0)) { (accumSeq, an) =>
+    accumSeq match {
+      case (a1, a2, counter) if a1 >= 3 || a2 >= 3 => (1, 1, counter + 1)
+      case (a1, a2, counter) if an < 0 && a1 < 3 => (a1 + 1, a2, counter)
+      case (a1, a2, counter) if an > 0 && a2 < 3 => (a1, a2 + 1, counter)
+
+    }
+  }
+  println(res1)
 
 }
 
